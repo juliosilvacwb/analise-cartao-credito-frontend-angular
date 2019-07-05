86ddd14bbc7556f87406cdf83f35b4c788d7c653
JULIO-WIN10 <julio.silva.cwb@gmail.com>
2019-07-05 16:44:25 -0300

    Ajuste path local

--- Dockerfile.dockerfile
+++ Dockerfile.dockerfile
@@ -2,8 +2,5 @@ FROM node
 MAINTAINER Julio Silva
 COPY . /var/www
 WORKDIR  /var/www
-RUN npm install -g @angular/cli
-RUN npm install
-RUN ng build --prod
 ENTRYPOINT npm start
 EXPOSE 3000
--- src/app/services/api.path.ts
+++ src/app/services/api.path.ts
@@ -1,4 +1,4 @@
 export class Api {
-    // static path: string = 'http://localhost:8080'
-    static path: string = 'http://create-card-backend:8080'
+    static path: string = 'http://localhost:8080'
+    // static path: string = 'http://create-card-backend:8080'
 }
