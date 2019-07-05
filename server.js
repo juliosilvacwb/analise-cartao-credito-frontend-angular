3459357b3ff169ed6ca836f9d654f819a03e5400
JULIO-WIN10 <julio.silva.cwb@gmail.com>
2019-07-05 16:15:45 -0300

    ajustes no pacote de instalação

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const baseDir = `${__dirname}/build/`
+const baseDir = `${__dirname}/dist/credito-card/`
 
 app.use(express.static(`${baseDir}`))
 
